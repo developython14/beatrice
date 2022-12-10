fb3a3a4daf1233150d6c34f8f2113998479a6e85
developython14 <djmel1955@gmail.com>
2022-12-10 02:38:30 +0100

    srtucturing the proect

--- plateforme/accounts/models.py
+++ plateforme/accounts/models.py
@@ -4,15 +4,16 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
-
 class Profile(AbstractUser):
-    nom = models.CharField(max_length=100 ,null=False )
-    prenom = models.CharField(max_length=100,null=False )
-    email = models.CharField(max_length=100,unique=True,default='khasarou@gmail.com')
-    password = models.CharField(max_length=100 )
-    numero_du_telephone = models.CharField(max_length=100,null=False )
-    institue = models.CharField(max_length=100,null=False )
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100 )
+    email = models.CharField(max_length=100,unique=True,null=True)
+    password = models.CharField(max_length=100,default='12345678')
+    numero_du_telephone = models.CharField(max_length=100,unique=True )
+    institue = models.CharField(max_length=100 )
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['prenom', 'institue' , 'password']
+    REQUIRED_FIELDS = []
+
     def __str__(self):
         return self.nom
